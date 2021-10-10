d422b7706e56d8ac471c0c1c551d22e78a04b256
brooke <brookejones406@gmail.com>
2021-10-10 15:48:56 -0500

    initialsetup

---
+++ assets/script.js
@@ -0,0 +1,18 @@
+//select tags in HTML to input data
+
+
+
+//read documentation of API
+// API ex: call http://api.openweathermap.org/data/2.5/forecast?id=524901&appid={API key}
+// ex 2: api.openweathermap.org/data/2.5/forecast?id=524901&appid=f4458c0fc59bb2bcd5a17322c84ac36e
+
+//make a variable to store you API key
+//APIkey = "f4458c0fc59bb2bcd5a17322c84ac36e";
+
+
+fetch('api.openweathermap.org/data/2.5/forecast?id=524901&appid=f4458c0fc59bb2bcd5a17322c84ac36e')
+
+.then(function(response) {
+    return response.json();
+})
+//will return resolved or rejected for a response.
