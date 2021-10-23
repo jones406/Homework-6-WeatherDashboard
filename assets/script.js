723b8cb3755d4b3294c8da15071aa9ef1ec51822
brooke <brookejones406@gmail.com>
2021-10-22 23:27:47 -0500

    APIcallwork

--- assets/script.js
+++ assets/script.js
@@ -1,24 +1,50 @@
-let APIkey = "f4458c0fc59bb2bcd5a17322c84ac36e";
-let city = "";
 
-//api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key} required parameters: q is the query parameter where we ad city variable and appid parameters is where we add the api key var
-
-let queryURL = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${APIKey}`;
-
+let weatherResults = document.getElementById("weatherdata");
+let cityFormEl = document.getElementById("cityname");
+let cityName = cityFormEl.value.trim(); //grab input from form and trim
+var submitButton = document.getElementById('btn');
 
 
-fetch('api.openweathermap.org/data/2.5/forecast?id=524901&appid=f4458c0fc59bb2bcd5a17322c84ac36e')
+let formSubmitHandler = function (event) {
+  event.preventDefault();
+  if (cityName) {
 
-.then(function(response) {
-    return response.json();
-})
-//will return resolved or rejected for a response.
+    getWeather(cityName)
+    cityFormEl.reset()
+    console.log(cityName); //not working
+  } else {
+    alert("Enter a city and click the 'Get Weather' button.");
+  }
+};
 
+//The one call API requires lat and long. So, user types in a city name, which is stored int he above cityName variable.
+//Take the city name and get it's lat and long? 
+//Input lat/long into the queryURL for the onecall API URL
 
-//use local storage to store persistent data\
+let getWeather = function (cityName) {
+  const APIkey = 'f4458c0fc59bb2bcd5a17322c84ac36e';
+  let queryURL = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${APIKey}`;
 
-//search for a city
+  fetch(queryURL)
+    .then(function (response) { //take the response from the API call
+      return response.json();  //convert response to JSON
+    }
+    )
+    .then(function (data) {
+      console.log(data);  //take converted data (now an array) and log it
+      for (let i = 0; i < data.length; i++) {
+        let weatherData = document.createElement('div');
+        weatherData.textContent = data[i];
+        weatherResults.appendChild(weatherData);
+      }
+    })
+}
+
+//TODOs:
 
 //get current AND future weather
-
 //return city name, date, icons rep of weather condition, temp, humidity, wind speed, UV index
+
+//use local storage to store persistent data
+
+submitButton.addEventListener('click', getWeather);
